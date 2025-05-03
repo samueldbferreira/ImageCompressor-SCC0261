d41ccd89fe6d9bede842cf209ecc7dd4664024fd
Samuel Ferreira <samueldbferreira@gmail.com>
2025-05-03 17:38:43 -0300

    feat: add list

--- compressor.c
+++ compressor.c
@@ -88,6 +88,90 @@ void loadBMPImage(FILE *input, BMPINFOHEADER InfoHeader, Pixel *Image){
   }
 }
 
+struct ListNode {
+  int difference;
+  int frequence;
+  struct ListNode* next;
+};
+
+typedef struct ListNode ListNode_t;
+
+struct List {
+  ListNode_t* first;
+  ListNode_t* last;
+};
+
+typedef struct List List_t;
+
+List_t* createList() {
+  List_t* list = (List_t*) malloc(sizeof(List_t));
+
+  if (list == NULL) {
+    printf("Invalid list (NULL) on createList.");
+    return NULL;
+  }
+
+  list->first = NULL;
+  list->last = NULL;
+
+  return list;
+};
+
+void listInsert(List_t* list, int difference) {
+  if (list == NULL) {
+    printf("Invalid list (NULL) for listInsert.");
+    return;
+  }
+
+  ListNode_t* newNode = (ListNode_t*) malloc(sizeof(ListNode_t));
+  newNode->difference = difference;
+  newNode->frequence = 0;
+  newNode->next = NULL;
+
+  if (list->first == NULL) {
+    list->first = newNode;
+    list->last = newNode;
+    return;
+  }
+
+  list->last->next = newNode;
+  list->last = newNode;
+}
+
+void printList(List_t* list) {
+  if (list == NULL) {
+    printf("Invalid list (NULL) for printList.");
+    return;
+  }
+
+  int i = 1;
+  ListNode_t* aux = list->first;
+  while (aux != NULL) {
+    printf("NODE %d: (diff > %i; freq > %i)\n", i, aux->difference, aux->frequence);
+
+    aux = aux->next;
+
+    i++;
+  }
+  printf("\n");
+}
+
+void destroyList(List_t* list) {
+  if (list == NULL) {
+    printf("Invalid list (NULL) for destroyList.");
+    return;
+  }
+
+  ListNode_t* aux = list->first;
+  while (aux != NULL) {
+    list->first = aux->next;
+    free(aux);
+    aux = list->first;
+  }
+
+  free(list);
+}
+
 int main() {
   char* inputFilePath = "./256x256.bmp";
   FILE* inputBmpFile = fopen(inputFilePath, "rb");
@@ -116,15 +200,24 @@ int main() {
   }
                   
   fseek(inputBmpFile, 0, SEEK_SET);
-      
+  
+  // Reescrevendo o cabeçalho do arquivo BMP
   for (int i = 0; i < 54; i++) {
     fputc(fgetc(inputBmpFile), bmpOutputFile);
   }
-      
-  for (int i = 0; i < (infoHeader.biHeight * infoHeader.biWidth); i++) {
-    fputc(Image[i].B, bmpOutputFile);
-    fputc(Image[i].G, bmpOutputFile);
-    fputc(Image[i].R, bmpOutputFile);
+
+  // Compressão simples: armazenando a diferença entre os pixels
+  // O primeiro pixel é armazenado diretamente
+  fputc(Image[0].B, bmpOutputFile);
+  fputc(Image[0].G, bmpOutputFile);
+  fputc(Image[0].R, bmpOutputFile);
+
+  // Armazenando a diferença entre os pixels consecutivos
+  // O primeiro pixel já foi armazenado, então começamos do segundo
+  for (int i = 1; i < (infoHeader.biHeight * infoHeader.biWidth); i++) {
+    fputc(Image[i].B - Image[i - 1].B, bmpOutputFile);
+    fputc(Image[i].G - Image[i - 1].G, bmpOutputFile);
+    fputc(Image[i].R - Image[i - 1].R, bmpOutputFile);
   }
 
   fclose(inputBmpFile);
